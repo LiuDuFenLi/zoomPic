c15c8b1e77d509342902050ca8afd1cf4814cf53
linianli <webyelow@126.com>
2017-08-01 18:18:00 +0800

    zoomPIC

---
+++ i.js
@@ -0,0 +1,52 @@
+var controlPic = {
+	zoomPic:function(){
+		var thisWidth,thisHeight,imgLeft,imgTop;
+		var imgWidth = 214;
+		var imgHeight = 310;
+		$('.detail .img').each(function(){
+			thisWidth = $(this).find('img').width();
+			thisHeight = $(this).find('img').height();
+			if(thisWidth > imgWidth && thisHeight > imgHeight){//如果宽，高都大
+				if(thisWidth/imgWidth > thisHeight/imgHeight){
+					imgLeft =  -((thisWidth / (thisHeight/imgHeight) - imgWidth) / 2);
+					$(this).find('img').css('height',imgHeight);
+					$(this).find('img').css('left',imgLeft);
+				}else{
+					imgTop =  -((thisHeight / (thisWidth/imgWidth) - imgHeight) / 2);	
+					$(this).find('img').css('width',imgWidth);
+					$(this).find('img').css('top',imgTop);
+				}
+			}else if(thisWidth < imgWidth && thisHeight < imgHeight){//如果宽，高都小
+				if(thisWidth/imgWidth > thisHeight/imgHeight){
+					imgLeft =  -((thisWidth / (thisHeight/imgHeight) - imgWidth) / 2);
+					$(this).find('img').css('height',imgHeight);
+					$(this).find('img').css('left',imgLeft);
+				}else{
+					imgTop =  -((thisHeight / (thisWidth/imgWidth) - imgHeight) / 2);	
+					$(this).find('img').css('width',imgWidth);
+					$(this).find('img').css('top',imgTop);
+				}	
+			}else if(thisWidth >= imgWidth && thisHeight < imgHeight){//如果宽等 大，高小
+				if(thisWidth/imgWidth > thisHeight/imgHeight){
+					imgLeft =  -((thisWidth / (thisHeight/imgHeight) - imgWidth) / 2);
+					$(this).find('img').css('height',imgHeight);
+					$(this).find('img').css('left',imgLeft);
+				}else{
+					imgTop =  -((thisHeight / (thisWidth/imgWidth) - imgHeight) / 2);	
+					$(this).find('img').css('width',imgWidth);
+					$(this).find('img').css('top',imgTop);
+				}	
+			}else if(thisWidth < imgWidth && thisHeight >= imgHeight){//如果高等 大，宽小
+				if(thisWidth/imgWidth > thisHeight/imgHeight){
+					imgLeft =  -((thisWidth / (thisHeight/imgHeight) - imgWidth) / 2);
+					$(this).find('img').css('height',imgHeight);
+					$(this).find('img').css('left',imgLeft);
+				}else{
+					imgTop =  -((thisHeight / (thisWidth/imgWidth) - imgHeight) / 2);	
+					$(this).find('img').css('width',imgWidth);
+					$(this).find('img').css('top',imgTop);
+				}	
+			}
+		});
+	}	
+}
