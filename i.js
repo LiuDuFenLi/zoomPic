6ef53ab75db2547b8219e00e26ff7f43fd18e638
linianli <webyellow@126.com>
2019-05-06 10:02:16 +0800

    t1

--- i.js
+++ i.js
@@ -13,7 +13,7 @@ var controlPic = {
 					$(this).find('img').css('height',imgHeight);
 					$(this).find('img').css('left',imgLeft);
 				}else{
-					imgTop =  -((thisHeight / (thisWidth/imgWidth) - imgHeight) / 2);	
+					imgTop =  -((thisHeight / (thisWidth/imgWidth) - imgHeight) / 2);
 					$(this).find('img').css('width',imgWidth);
 					$(this).find('img').css('top',imgTop);
 				}
@@ -23,31 +23,32 @@ var controlPic = {
 					$(this).find('img').css('height',imgHeight);
 					$(this).find('img').css('left',imgLeft);
 				}else{
-					imgTop =  -((thisHeight / (thisWidth/imgWidth) - imgHeight) / 2);	
+					imgTop =  -((thisHeight / (thisWidth/imgWidth) - imgHeight) / 2);
 					$(this).find('img').css('width',imgWidth);
 					$(this).find('img').css('top',imgTop);
-				}	
+				}
 			}else if(thisWidth >= imgWidth && thisHeight < imgHeight){//如果宽等 大，高小
 				if(thisWidth/imgWidth > thisHeight/imgHeight){
 					imgLeft =  -((thisWidth / (thisHeight/imgHeight) - imgWidth) / 2);
 					$(this).find('img').css('height',imgHeight);
 					$(this).find('img').css('left',imgLeft);
 				}else{
-					imgTop =  -((thisHeight / (thisWidth/imgWidth) - imgHeight) / 2);	
+					imgTop =  -((thisHeight / (thisWidth/imgWidth) - imgHeight) / 2);
 					$(this).find('img').css('width',imgWidth);
 					$(this).find('img').css('top',imgTop);
-				}	
+				}
 			}else if(thisWidth < imgWidth && thisHeight >= imgHeight){//如果高等 大，宽小
 				if(thisWidth/imgWidth > thisHeight/imgHeight){
 					imgLeft =  -((thisWidth / (thisHeight/imgHeight) - imgWidth) / 2);
 					$(this).find('img').css('height',imgHeight);
 					$(this).find('img').css('left',imgLeft);
 				}else{
-					imgTop =  -((thisHeight / (thisWidth/imgWidth) - imgHeight) / 2);	
+					imgTop =  -((thisHeight / (thisWidth/imgWidth) - imgHeight) / 2);
 					$(this).find('img').css('width',imgWidth);
 					$(this).find('img').css('top',imgTop);
-				}	
+				}
 			}
 		});
-	}	
+	}
 }
+//bbb
